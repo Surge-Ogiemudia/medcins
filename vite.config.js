79f8e70d695f6f2a7973373fe0a766327a8765d8
Your Name <you@example.com>
2025-10-29 12:14:30 +0100

    Save changes before gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pharmastack/' : '/',
+  base: mode === 'production' ? '/medcins/' : '/',
   plugins: [react()],
   server: {
     allowedHosts: [
